7f07a915ee893e26689b8aa2ccf14656b8d8d60a
Satish Venkatakrishna <satish860@gmail.com>
2018-07-23 14:19:33 +0530

    Database Migration

---
+++ src/ContosoUniversity.DbMigration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.DbMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
