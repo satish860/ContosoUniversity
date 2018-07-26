fdceded2958521bf5beabda777ba1baff558bcc4
Satish Venkatakrishna <satish860@gmail.com>
2018-07-26 12:26:31 +0530

    Database Migration done and still need to polish . but will do it later

--- src/ContosoUniversity.DbMigration/Program.cs
+++ src/ContosoUniversity.DbMigration/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DbUp;
 
 namespace ContosoUniversity.DbMigration
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+
+            var connectionString =
+        args.FirstOrDefault()
+        ?? "Data Source=PRO-LP116;Initial Catalog=ContosoUniversity;Integrated Security=True";
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
