1f7213d6c44a7f9390ce975b5cfb62587583b501
Satish Venkatakrishna <satish860@gmail.com>
2018-07-27 09:40:28 +0530

    Finished details test . Super cool way to do testing with Respwan

---
+++ tests/ContosoUniversity.IntegerationTests/Students/DetailsTests.cs
@@ -0,0 +1,30 @@
+﻿using ContosoUniversity.Domain.Students;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static ContosoUniversity.IntegerationTests.SliceFixture;
+using FluentAssertions;
+
+namespace ContosoUniversity.IntegerationTests.Students
+{
+    public class DetailsTests : IntegerationTestBase
+    {
+        [Fact]
+        public async Task ShouldBeAbleToGetDetails()
+        {
+            var createStudentCommand = new CreateStudentCommand
+            {
+                FirstName = "satish",
+                LastName = "Venkatakrishnan",
+                EnrollmentDate = DateTime.Now
+            };
+            var id = await SendAsync(createStudentCommand);
+            var details = await SendAsync(new DetailsModel { Id = id });
+            details.FirstName.Should().Be("satish");
+
+        }
+    }
+}
